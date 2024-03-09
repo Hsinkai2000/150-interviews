dfab9b5bbc05578349978e03726b2ef10dc71f96
Hsin Kai <hsinkai2000@gmail.com>
2024-03-10 00:14:32 +0800

    Day 10 - longest consecutive
    Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
    
    You must write an algorithm that runs in O(n) time.

---
+++ src/Python/HashMap/Medium/longestConsecutive.py
@@ -0,0 +1,49 @@
+from collections import Counter
+
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        # if not nums:
+        #     return 0
+        # numsCounter = Counter(nums)
+        # numsCounter = sorted(numsCounter.items())
+        # current = numsCounter[0][0]
+        # currCount = 1
+        # count = 1
+        # for i in range (1, len(numsCounter)):
+        #     numi = numsCounter[i][0]
+        #     if numi - 1 == current:
+        #         count +=1
+        #     else:
+        #         count = 1            
+        #     current = numi
+        #     if currCount < count:
+        #         currCount = count
+        # return currCount
+    
+        if not nums:
+            return 0
+        numSet = set(nums)
+        numSet = sorted(numSet)
+        current = numSet[0]
+        currCount = 1
+        count = 1
+
+        for i in range (1, len(numSet)):
+            numi = numSet[i]
+            if numi - 1 == current:
+                count +=1
+            else:
+                count = 1            
+            current = numi
+            if currCount < count:
+                currCount = count
+        return currCount
+
+    
+s = Solution()
+print(s.longestConsecutive([100,4,200,1,3,2]))
+print(s.longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+print(s.longestConsecutive([1,2,0,1]))
+print(s.longestConsecutive([0]))
+print(s.longestConsecutive([0,0]))
+print(s.longestConsecutive([]))
